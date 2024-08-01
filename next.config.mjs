98f7a5199d2ffc95aa480870a1ce4b019251004f
rpangarego <rpangarego@gmail.com>
2024-08-01 22:33:58 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config, { isServer }) {
+    // Apply the topLevelAwait experiment only if not running on the server
+    if (!isServer) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
